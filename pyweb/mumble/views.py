a5bd43cdf89bc1c5f952d069f851eac0c8b5c860
root <root@sphynx.funzt-halt.net>
2009-01-25 14:53:25 +0100

    Fixed a bug that prevented the Channel list from working for anonymous users

--- pyweb/mumble/views.py
+++ pyweb/mumble/views.py
@@ -1,3 +1,35 @@
+""" This file is part of the mumble-django application.
+    
+    Copyright (C) 2009, Michael Svedrin Ziegler <diese-addy@funzt-halt.net>
+ 
+    All rights reserved.
+ 
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+ 
+    - Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    - Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    - Neither the name of the Mumble Developers nor the names of its
+      contributors may be used to endorse or promote products derived from this
+      software without specific prior written permission.
+ 
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 from django.shortcuts				import render_to_response, get_object_or_404, get_list_or_404
 from django.template				import RequestContext
 from django.http					import HttpResponseRedirect
@@ -30,7 +62,7 @@ def showContent( server, user = None ):
 	srv, o = createChannelList( server );
 	
 	mumbleAcc = None;
-	if user is not None:
+	if user.is_authenticated():
 		mmUsers = MumbleUser.objects.filter( owner = user );
 		if mmUsers:
 			mumbleAcc = mmUsers[0];
