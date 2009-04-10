ef37b1cb9f026087aea97816d38e57dfb42abbcc
Michael Ziegler <Michael Ziegler@localhost>
2009-04-10 15:23:03 +0200

    corrected whitespace

--- pyweb/urls.py
+++ pyweb/urls.py
@@ -8,20 +8,18 @@ from django.conf import settings
 
 urlpatterns = patterns('',
 	(r'^/?$',               'django.views.generic.simple.redirect_to', { 'url': '/mumble/' } ),
-    # Example:
-    # (r'^mumble_django/', include('mumble_django.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+	# to INSTALLED_APPS to enable admin documentation:
+	# (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    (r'^accounts/profile/', 'views.profile' ),
-    (r'^accounts/',         include('registration.urls')),
+	(r'^accounts/profile/', 'views.profile' ),
+	(r'^accounts/',         include('registration.urls')),
 
-    (r'^mumble/',           include('mumble.urls')),
+	(r'^mumble/',           include('mumble.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+	# Uncomment the next line to enable the admin:
+	(r'^admin/(.*)', admin.site.root),
 )
 
 # Development stuff
