605a66ddf7aade86eada037800eb3b87df4e39eb
Michael Ziegler <diese-addy@funzt-halt.net>
2009-06-10 17:42:52 +0200

    a few refurbishments, more are yet to come

--- pyweb/mumble/MumbleCtlDbus.py
+++ pyweb/mumble/MumbleCtlDbus.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
-# mumble-django contributed by withgod@sourceforge.net
+
+"""
+ *  Copyright (C) 2009, withgod                   <withgod@sourceforge.net>
+ *                      Michael "Svedrin" Ziegler <diese-addy@funzt-halt.net>
+ *
+ *  Mumble-Django is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
 
 from PIL    import Image
 from struct import pack, unpack
