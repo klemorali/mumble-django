cc27771899a79a9d5267820a0a7c0bc2451eb5b7
Michael Ziegler <diese-addy@funzt-halt.net>
2009-09-01 00:11:44 +0200

    auto-detect the script prefix path under which mumble-django is running

--- pyweb/__init__.py
+++ pyweb/__init__.py
@@ -12,3 +12,17 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
 """
+
+from django.core import signals
+
+def update_paths( **kwargs ):
+	from django.core.urlresolvers import get_script_prefix, reverse
+	from os.path     import join
+	from django.conf import settings
+	pf = get_script_prefix();
+	settings.MEDIA_URL          = "%sstatic" % pf;
+	settings.ADMIN_MEDIA_PREFIX = "%smedia"  % pf;
+	settings.LOGIN_URL          = reverse( "django.contrib.auth.views.login" );
+	signals.request_started.disconnect( update_paths );
+
+signals.request_started.connect( update_paths );
