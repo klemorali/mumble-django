3d88d2eb6fb91b1ddd83362bc1bca42dd4e13edf
Michael Ziegler <diese-addy@funzt-halt.net>
2009-11-29 14:53:32 +0100

    change hardcoded indices to use the slice enum correctly

--- pyweb/mumble/MumbleCtlIce.py
+++ pyweb/mumble/MumbleCtlIce.py
@@ -300,11 +300,13 @@ class MumbleCtlIce_120(MumbleCtlIce_118):
 		return ret
 	
 	def registerPlayer(self, srvid, name, email, password):
+		# To get the real values of these ENUM entries, try
+		# Murmur.UserInfo.UserX.value
 		from Murmur import UserInfo
 		user = {
-			UserInfo(0): name.encode( "UTF-8" ),
-			UserInfo(1): email.encode( "UTF-8" ),
-			UserInfo(4): password.encode( "UTF-8" ),
+			UserInfo.UserName:     name.encode( "UTF-8" ),
+			UserInfo.UserEmail:    email.encode( "UTF-8" ),
+			UserInfo.UserPassword: password.encode( "UTF-8" ),
 			};
 		return self._getIceServerObject(srvid).registerUser( user );
 	
@@ -317,9 +319,9 @@ class MumbleCtlIce_120(MumbleCtlIce_118):
 	def setRegistration(self, srvid, mumbleid, name, email, password):
 		from Murmur import UserInfo
 		user = {
-			UserInfo(0): name.encode( "UTF-8" ),
-			UserInfo(1): email.encode( "UTF-8" ),
-			UserInfo(4): password.encode( "UTF-8" ),
+			UserInfo.UserName:     name.encode( "UTF-8" ),
+			UserInfo.UserEmail:    email.encode( "UTF-8" ),
+			UserInfo.UserPassword: password.encode( "UTF-8" ),
 			};
 		return self._getIceServerObject( srvid ).updateRegistration( mumbleid, user )
 	
