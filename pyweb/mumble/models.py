a8af98c4ecefcef505d2a98f5f0ebf3d057ce1eb
Michael Ziegler <diese-addy@funzt-halt.net>
2010-02-21 17:47:42 +0100

    fix "new user" dialog being broken by getAdmin trying to access a not-yet-set ForeignKey.

--- pyweb/mumble/models.py
+++ pyweb/mumble/models.py
@@ -537,10 +537,15 @@ class MumbleUser( models.Model ):
 	# Admin handlers
 	def getAdmin( self ):
 		""" Get ACL of root Channel, get the admin group and see if this user is in it. """
-		return self.server.rootchan.acl.group_has_member( "admin", self.mumbleid );
+		if self.mumbleid == -1:
+			return False;
+		else:
+			return self.server.rootchan.acl.group_has_member( "admin", self.mumbleid );
 	
 	def setAdmin( self, value ):
 		""" Set or revoke this user's membership in the admin group on the root channel. """
+		if self.mumbleid == -1:
+			return False;
 		if value:
 			self.server.rootchan.acl.group_add_member( "admin", self.mumbleid );
 		else:
