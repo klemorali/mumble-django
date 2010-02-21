36911774ea3bce67fdd58ab87f2246e830c6be9a
Michael Ziegler <diese-addy@funzt-halt.net>
2010-02-21 16:41:24 +0100

    sqlite: create a new table with the proper schema to get rid of the old fields.

--- pyweb/mumble/conversionsql/sqlite/01-schema-mumble-server_id.sql
+++ pyweb/mumble/conversionsql/sqlite/01-schema-mumble-server_id.sql
@@ -1,2 +1,11 @@
-ALTER TABLE "mumble_mumble"
-        ADD "server_id" integer;
+CREATE TABLE "mumble_mumble_new" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "server_id" integer NOT NULL REFERENCES "mumble_mumbleserver" ("id"),
+    "name" varchar(200) NOT NULL,
+    "srvid" integer NOT NULL,
+    "addr" varchar(200) NOT NULL,
+    "port" integer NOT NULL,
+    "display" varchar(200) NOT NULL,
+    UNIQUE ("server_id", "srvid"),
+    UNIQUE ("addr", "port")
+);
