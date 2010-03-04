320c819f19016e46fcddf86a4018f2bcb5c16195
Michael Ziegler <diese-addy@funzt-halt.net>
2010-03-04 08:59:16 +0100

    move the database file to a db subdirectory, to get the perms right when people run "chmod -R <md-dir>/*"

--- pyweb/mumble/management/__init__.py
+++ pyweb/mumble/management/__init__.py
@@ -14,6 +14,10 @@
  *  GNU General Public License for more details.
 """
 
+from shutil		import copy, move
+from os.path		import exists, join
+
+from django.conf	import settings
 from django.db		import connection
 from django.db.models	import signals
 
@@ -21,6 +25,14 @@ from mumble		import models
 
 from update_schema	import update_schema
 from server_detect	import find_existing_instances
+
+
+if settings.DATABASE_ENGINE == "sqlite3":
+	# Move the DB to the db subdirectory if necessary.
+	oldpath = join( settings.MUMBLE_DJANGO_ROOT, "mumble-django.db3" )
+	if not exists( settings.DATABASE_NAME ) and exists( oldpath ):
+		move( oldpath, settings.DATABASE_NAME )
+
 
 cursor = connection.cursor()
 
@@ -38,6 +50,9 @@ else:
 	uptodate = True
 
 if not uptodate:
+	if settings.DATABASE_ENGINE == "sqlite3":
+		# backup the db before the conversion.
+		copy( settings.DATABASE_NAME, settings.DATABASE_NAME+".bak" )
 	signals.post_syncdb.connect( update_schema, sender=models );
 else:
 	signals.post_syncdb.connect( find_existing_instances, sender=models );
