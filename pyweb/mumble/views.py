f4e59fedda87e0f587368c68cfbf986018ac982e
Michael Ziegler <diese-addy@funzt-halt.net>
2010-07-30 18:24:24 +0200

    properly handle log retrieval in the model and add filtering functionality

--- pyweb/mumble/views.py
+++ pyweb/mumble/views.py
@@ -207,14 +207,14 @@ def get_admin( request, server ):
 
 @login_required
 @EXT_DIRECT_PROVIDER.register_method( "Mumble" )
-def log( request, server, start, limit ):
+def log( request, server, start, limit, filter ):
     """ Retrieve log messages. """
     srv = get_object_or_404( Mumble, id=int(server) )
     if not srv.isUserAdmin( request.user ):
         raise Exception( "Access denied" )
     return { 'data': [
             { 'timestamp': ent.timestamp, 'txt': ent.txt }
-            for ent in srv.ctl.getLog( srv.srvid, start, (start + limit) )
+            for ent in srv.getLog( start, (start + limit), filter )
         ], 'success': True }
 
 
