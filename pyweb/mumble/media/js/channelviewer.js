271f9c2eda84b46d1f63f2db36b27d88d3062a48
Michael Ziegler <diese-addy@funzt-halt.net>
2010-08-24 14:59:01 +0200

    add copyright/licensing stuff and link to the documentation

--- pyweb/mumble/media/js/channelviewer.js
+++ pyweb/mumble/media/js/channelviewer.js
@@ -1,5 +1,22 @@
 // kate: space-indent on; indent-width 4; replace-tabs on;
 
+/**
+ *  Copyright © 2010, Michael "Svedrin" Ziegler <diese-addy@funzt-halt.net>
+ *
+ *  Mumble-Django is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Documentation for this channel viewer can be found here:
+ *    http://mumble-django.org/docs/api/channelviewer.html
+ */
+
 Ext.namespace('Ext.ux');
 
 if( typeof gettext == "undefined" ){
