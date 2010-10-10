d16e9bb7803900fe3a30a5094760bc480acf59dd
Michael Ziegler <diese-addy@funzt-halt.net>
2010-10-10 20:18:05 +0200

    Bump mumble module version

--- pyweb/mumble/__init__.py
+++ pyweb/mumble/__init__.py
@@ -14,7 +14,7 @@
  *  GNU General Public License for more details.
 """
 
-version = { 'major': 2, 'minor': 4, 'beta': 2 }
+version = { 'major': 2, 'minor': 4, 'beta': 3 }
 
 if version['beta']:
     version_str = "v%(major)d.%(minor)dbeta%(beta)d" % version
