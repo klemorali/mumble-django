c2a4b0b22676b4e1019b7c6a6843f54fe7a69082
Michael Ziegler <diese-addy@funzt-halt.net>
2010-10-22 21:32:57 +0200

    add view for an iframe embeddable channel viewer page

--- pyweb/mumble/urls.py
+++ pyweb/mumble/urls.py
@@ -37,6 +37,8 @@ urlpatterns = patterns(
     ( r'mobile/(?P<server>\d+)/?$',          'mobile_show'     ),
     ( r'mobile/?$',                          'mobile_mumbles'  ),
 
+    ( r'embed/(?P<server>\d+)/?$',           'embed'            ),
+
     ( r'(?P<server>\d+).json',               'cvp_json'        ),
     ( r'(?P<server>\d+).xml',                'cvp_xml'         ),
 
