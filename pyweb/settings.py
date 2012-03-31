d63c2639cd6d49618a604afdc14d19ab5641ed71
Michael Ziegler <diese-addy@funzt-halt.net>
2012-03-31 18:42:40 +0200

    adapt settings for Django 1.4

--- pyweb/settings.py
+++ pyweb/settings.py
@@ -230,8 +230,9 @@ except NameError:
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -257,7 +258,7 @@ TEMPLATE_DIRS.extend((
 ))
 
 TEMPLATE_CONTEXT_PROCESSORS = [
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
