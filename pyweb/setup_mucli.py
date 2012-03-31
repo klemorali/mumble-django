d15a7f95439e65f9bfd419de55bbc244b583491a
Michael Ziegler <diese-addy@funzt-halt.net>
2012-03-31 18:44:36 +0200

    Bump version in setup_mucli.py

--- pyweb/setup_mucli.py
+++ pyweb/setup_mucli.py
@@ -18,7 +18,7 @@
 from distutils.core import setup
 
 setup(name='mucli',
-      version="2.5",
+      version="2.6",
       description="Murmur Command Line Interface using Mumble-Django's connectors",
       author="Michael Ziegler",
       author_email='diese-addy@funzt-halt.net',
